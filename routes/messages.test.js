74f26a75df28d542a46cb277e5aee9380cc2f904
Andrew Smith <meuzishun@gmail.com>
2023-08-14 11:04:19 -0700

    Refactor existence tests for messages

--- routes/messages.test.js
+++ routes/messages.test.js
@@ -4,16 +4,16 @@ const request = require('supertest');
 describe('Message routes', () => {
   test('Messages route exists', async () => {
     const res = await request(app).get('/messages');
-    expect(res.status).toBe(200);
+    expect(res.status).not.toBe(404);
   });
 
   test('Single message route exists', async () => {
     const res = await request(app).get('/messages/123');
-    expect(res.status).toBe(200);
+    expect(res.status).not.toBe(404);
   });
 
   test('New message route exists', async () => {
     const res = await request(app).post('/messages/new');
-    expect(res.status).toBe(200);
+    expect(res.status).not.toBe(404);
   });
 });
