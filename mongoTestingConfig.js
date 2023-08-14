258374b2ce6726c16b97c4c92b75a0ef46ea4c45
Andrew Smith <meuzishun@gmail.com>
2023-08-14 09:59:42 -0700

    Create mongoTestingConfig with mongodb-memory-server

---
+++ mongoTestingConfig.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const initializeMongoServer = async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  const mongoURI = mongoServer.getUri();
+
+  mongoose.connect(mongoURI);
+
+  mongoose.connection.on('error', (err) => {
+    if (err.message.code === 'ETIMEDOUT') {
+      console.log(err);
+      mongoose.connect(mongoURI);
+    }
+    console.log(err);
+  });
+
+  mongoose.connection.once('open', () => {
+    console.log(`Mocked MongoDB successfully connected to ${mongoURI}`);
+  });
+};
+
+module.exports = initializeMongoServer;
