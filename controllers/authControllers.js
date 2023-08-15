6c3957c7f8c78bd0ed099cff23cf8e646a64f97f
Andrew Smith <meuzishun@gmail.com>
2023-08-15 14:06:12 -0700

    postRegister controller basic

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,30 @@
-const postRegister = (req, res) => {
-  return res.send('Register POST route');
+const User = require('../models/user');
+
+const postRegister = async (req, res) => {
+  if (!req.body.user) {
+    return res.status(400).json({ msg: 'No user sent' });
+  }
+  const { firstName, lastName, email } = req.body.user;
+
+  const userSearch = await User.findOne({ email });
+  if (userSearch) {
+    return res.status(400).json({ msg: 'User already exists' });
+  }
+
+  if (!firstName) {
+    return res.status(400).json({ msg: 'No first name' });
+  }
+
+  if (!lastName) {
+    return res.status(400).json({ msg: 'No last name' });
+  }
+
+  if (!email) {
+    return res.status(400).json({ msg: 'No email' });
+  }
+
+  const user = await User.create(req.body.user);
+  return res.status(201).json({ user });
 };
 
 const postLogin = (req, res) => {
