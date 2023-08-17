e5278504b05f3f39d733122f93d7bada339de009
Andrew Smith <meuzishun@gmail.com>
2023-08-17 01:29:52 -0400

    Include errorHandler in app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes/index');
+const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', routes);
+app.use(errorHandler);
 
 module.exports = app;
