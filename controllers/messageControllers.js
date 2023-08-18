787058549c60b0ef64fd15074e36c57caf673d45
Andrew Smith <meuzishun@gmail.com>
2023-08-18 09:54:42 -0400

    Alter messageControllers to pass first tests

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -1,14 +1,39 @@
-const getMessages = (req, res) => {
-  res.send('Messages route GET');
-};
+const asyncHandler = require('express-async-handler');
+const Message = require('../models/message');
 
-const getMessage = (req, res) => {
-  res.send(`Message ${req.params.messageId} route GET`);
-};
+const getMessages = asyncHandler(async (req, res) => {
+  const messages = await Message.find({});
+  return res.status(200).json({ messages });
+});
 
-const postNewMessage = (req, res) => {
-  res.send('New message route POST');
-};
+const getMessage = asyncHandler(async (req, res) => {
+  const message = await Message.findById(req.params.messageId);
+  if (!message) {
+    res.status(404);
+    throw new Error();
+  }
+  return res.status(200).json({ message });
+});
+
+const postNewMessage = asyncHandler(async (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    res.status(400);
+    throw new Error('No message submitted');
+  }
+
+  if (!req.body.content) {
+    res.status(400);
+    throw new Error('No message content submitted');
+  }
+
+  if (!req.body.author) {
+    res.status(400);
+    throw new Error('No author submitted');
+  }
+
+  const message = await Message.create({ ...req.body, timestamp: new Date() });
+  return res.status(201).json({ message });
+});
 
 module.exports = {
   getMessages,
