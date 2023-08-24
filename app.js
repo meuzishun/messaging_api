fc2d28118a592ab989c9bfe2960f49dad152cff5
Andrew Smith <meuzishun@gmail.com>
2023-08-24 16:04:33 -0400

    Add generateKeyPair file for creating auth keys; add to app.js; ignore files generated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const genKeyPair = require('./lib/generateKeyPair');
 const routes = require('./routes/index');
 const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
+genKeyPair();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
