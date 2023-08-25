49281b7c0c0062b7a99d97c69438eff28902187f
Andrew Smith <meuzishun@gmail.com>
2023-08-25 15:42:53 -0400

    Conditionally include limiter, compression, helmet, and cors in app

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 const genKeyPair = require('./lib/generateKeyPair');
 const routes = require('./routes/index');
 const { errorHandler } = require('./middleware/errorHandler');
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
+const whitelist = [
+  'https://meuzishun.github.io/blog-client-author',
+  'https://meuzishun.github.io/blog-client',
+  'https://meuzishun.github.io',
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 const app = express();
 genKeyPair();
+
+if (process.env.NODE_ENV === 'Production') {
+  app.use(limiter);
+  app.use(compression());
+  app.use(helmet());
+  app.use(cors(corsOptions));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
