6b5199a4809529aafb01eb63b2ff6d643322920d
Andrew Smith <meuzishun@gmail.com>
2023-08-25 15:13:43 -0400

    Remove try/catch block and clean up authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -16,25 +16,17 @@ const authMiddleware = asyncHandler(async (req, res, next) => {
     throw new Error('Not authorized, no token');
   }
 
-  try {
-    const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.verify(token, PRIV_KEY);
+  const user = await User.findById(decoded.id).select('-password');
 
-    const decoded = jwt.verify(token, PRIV_KEY);
-
-    const user = await User.findById(decoded.id).select('-password');
-
-    if (!user) {
-      res.status(401);
-      throw new Error('Not authorized, no user found');
-    }
-
-    req.body.user = user;
-
-    next();
-  } catch (error) {
+  if (!user) {
     res.status(401);
-    throw new Error('Not authorized');
+    throw new Error('Not authorized, no user found');
   }
+
+  req.body.user = user;
+  next();
 });
 
 module.exports = authMiddleware;
