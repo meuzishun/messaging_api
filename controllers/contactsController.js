b13fda70111528c703bcc7b265fabcc0b6ed8fe1
Andrew Smith <meuzishun@gmail.com>
2023-09-18 19:46:18 -0400

    Add validation to body and params of contact routes checking for valid mongo ids

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,3 +1,4 @@
+const { body, param, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const User = require('../models/user');
 
@@ -23,72 +24,108 @@ const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ contacts });
 });
 
-// @desc    Post new contact
-// @route   POST /api/contacts
+// @desc    Add new contact
+// @route   PUT /api/contacts
 // @access  Private
-const addContact = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.body.user._id);
+const addContact = [
+  body('contactId').isMongoId().withMessage('Invalid contact ID'),
 
-  if (!user) {
-    res.status(404);
-    throw new Error('No user found');
-  }
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  const updatedUser = await User.findByIdAndUpdate(
-    user._id,
-    { $push: { friends: req.body.contactId } },
-    { returnDocument: 'after' }
-  );
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      res.status(400);
+      throw new Error(errorMessages[0]);
+    }
 
-  res.status(201).json({ user: updatedUser });
-});
+    const user = await User.findById(req.body.user._id);
+
+    if (!user) {
+      res.status(404);
+      throw new Error('No user found');
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      user._id,
+      { $push: { friends: req.body.contactId } },
+      { returnDocument: 'after' }
+    );
+
+    res.status(201).json({ user: updatedUser });
+  }),
+];
 
 // @desc    Get contact
 // @route   GET /api/contacts/:contactId
 // @access  Private
-const getContact = asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-    _id: req.body.user._id,
-    friends: { $in: [req.params.contactId] },
-  });
+const getContact = [
+  param('contactId').isMongoId().withMessage('Invalid contact ID'),
 
-  if (!user) {
-    res.status(400);
-    throw new Error('Contact not friend');
-  }
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  const contact = await User.findById(req.params.contactId);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      res.status(400);
+      throw new Error(errorMessages[0]);
+    }
 
-  res.status(200).json({ contact });
-});
+    const user = await User.findOne({
+      _id: req.body.user._id,
+      friends: { $in: [req.params.contactId] },
+    });
+
+    if (!user) {
+      res.status(400);
+      throw new Error('Contact not friend');
+    }
+
+    const contact = await User.findById(req.params.contactId);
+
+    res.status(200).json({ contact });
+  }),
+];
 
 // @desc    Delete contact
 // @route   DELETE /api/contacts/:contactId
 // @access  Private
-const deleteContact = asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-    _id: req.body.user._id,
-    friends: { $in: [req.params.contactId] },
-  });
+const deleteContact = [
+  param('contactId').isMongoId().withMessage('Invalid contact ID'),
 
-  if (!user) {
-    res.status(400);
-    throw new Error('Contact not friend');
-  }
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
 
-  const updatedUser = await User.findOneAndUpdate(
-    { _id: req.body.user._id },
-    { $pull: { friends: req.params.contactId } },
-    { new: true }
-  );
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      res.status(400);
+      throw new Error(errorMessages[0]);
+    }
 
-  if (!updatedUser) {
-    res.status(500);
-    throw new Error('Updated user error');
-  }
+    const user = await User.findOne({
+      _id: req.body.user._id,
+      friends: { $in: [req.params.contactId] },
+    });
 
-  res.status(201).json({ user: updatedUser });
-});
+    if (!user) {
+      res.status(400);
+      throw new Error('Contact not friend');
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: req.body.user._id },
+      { $pull: { friends: req.params.contactId } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      res.status(500);
+      throw new Error('Updated user error');
+    }
+
+    res.status(201).json({ user: updatedUser });
+  }),
+];
 
 module.exports = {
   getContacts,
