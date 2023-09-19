1362527a14d74159143c2906547b8acfe3c831ea
Andrew Smith <meuzishun@gmail.com>
2023-09-19 08:45:49 -0400

    Create basic user controller with temporary functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,26 @@
+// @desc    Search users
+// @route   GET /api/users/search
+// @access  Private
+const searchUsers = async (req, res) => {
+  res.json({ msg: 'Searching users...' });
+};
 
+// @desc    Get single user
+// @route   GET /api/users/:userId
+// @access  Private
+const getUser = async (req, res) => {
+  res.json({ msg: 'Get single user' });
+};
+
+// @desc    Get users
+// @route   GET /api/users
+// @access  Private
+const getUsers = async (req, res) => {
+  res.json({ msg: 'Get all users' });
+};
+
+module.exports = {
+  searchUsers,
+  getUser,
+  getUsers,
+};
