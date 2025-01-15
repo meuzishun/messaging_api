298ca45488678a1d5cf346ac5dbc20f75aefa832
Andrew Smith <meuzishun@gmail.com>
2025-01-15 13:12:13 -0500

    Remove log in loginUser controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,7 +92,6 @@ const loginUser = [
   body('data.password').notEmpty().withMessage('No password'),
 
   asyncHandler(async (req, res) => {
-    console.log(req.body);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
